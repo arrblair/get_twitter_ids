8f75c7714c194be46d52dac61562ac15677be1b2
arrblair <ryan@livefyre.com>
2013-10-25 20:57:03 -0700

    add re & script now works to grab ids

--- find_twitter_ids.py
+++ find_twitter_ids.py
@@ -1,14 +1,13 @@
+import re
 import requests
 from sys import argv
 
 
 r = requests.get(argv[1])
 text = r.text
-parse_me = text.split('</')
 
-for element in parse_me:
-    if 'data-item-id' in element:
-        splitter = element.split('=')
-        number = splitter[1]
-        split_whole_id = whole_id.split('')
-        print number
+ids = re.findall(r'data-item-id="\d*', r.text)
+for id in ids:
+    splitter = id.split('"')
+    number = splitter[1]
+    print number
