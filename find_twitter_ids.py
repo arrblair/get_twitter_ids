4cc78fea34eaa51508fca9ac6e78a857145763e3
arrblair <ryan@livefyre.com>
2013-10-25 21:01:47 -0700

    add id numbers to array for future use

--- find_twitter_ids.py
+++ find_twitter_ids.py
@@ -4,10 +4,15 @@ from sys import argv
 
 
 r = requests.get(argv[1])
-text = r.text
 
 ids = re.findall(r'data-item-id="\d*', r.text)
+
+id_numbers = []
+
 for id in ids:
     splitter = id.split('"')
     number = splitter[1]
+    id_numbers.append(number)
+
+for number in id_numbers:
     print number
