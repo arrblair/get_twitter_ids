ea3a7c5d52b8050bcac6ba2a00e2474e44a6963f
arrblair <ryan@livefyre.com>
2013-10-25 19:54:31 -0700

    add grab twitter ids script

---
+++ find_twitter_ids.py
@@ -0,0 +1,14 @@
+import requests
+from sys import argv
+
+
+r = requests.get(argv[1])
+text = r.text
+parse_me = text.split('</')
+
+for element in parse_me:
+    if 'data-item-id' in element():
+        splitter = element.split('=')
+        number = splitter[1]
+        split_whole_id = whole_id.split('')
+        print number
