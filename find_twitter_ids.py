f55f1bfdee01499a3bc66fc89cf8b2cda25f9e2b
Ryan Blair <rhblair5d@gmail.com>
2013-11-18 19:02:43 -0800

    Update find_twitter_ids.py

--- find_twitter_ids.py
+++ find_twitter_ids.py
@@ -2,7 +2,7 @@ import re
 import requests
 from sys import argv
 
-
+# pass in the url that you need to get the twitter ids from the command line
 r = requests.get(argv[1])
 
 ids = re.findall(r'data-item-id="\d*', r.text)
